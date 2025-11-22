90df567cabd15729737fc998f7f40eae78cf2f68
Emil Rinagl <emil.rinagl@student.unisg.ch>
2025-11-22 13:36:52 +0100

    test commit

--- Home.py
+++ Home.py
@@ -1,11 +1,11 @@
 
 import streamlit as st
 
-from src.core.layout import sidebar_footer
-sidebar_footer()
-
 st.set_page_config(page_title="Donor Analytics", page_icon="🎯", layout="wide")
 st.title("🎯 Donor Analytics")
+
+from src.core.layout import sidebar_footer
+sidebar_footer()
 
 st.write(
     "Welcome! Use the left sidebar to navigate to **Segmentation**, **Churn**, or **LTV**.\n\n"
